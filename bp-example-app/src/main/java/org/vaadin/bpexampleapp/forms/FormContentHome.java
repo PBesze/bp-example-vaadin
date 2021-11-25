dc9f56769c26e0638703c8f704b08f9b23371e41
Peter Besze <pbesze@gmail.com>
2021-11-25 10:16:54 +0100

    Update FormContentHome.java

--- bp-example-app/src/main/java/org/vaadin/bpexampleapp/forms/FormContentHome.java
+++ bp-example-app/src/main/java/org/vaadin/bpexampleapp/forms/FormContentHome.java
@@ -1,42 +1,19 @@
 package org.vaadin.bpexampleapp.forms;
 
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.HasComponents;
-import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.orderedlayout.FlexLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.select.Select;
-import com.vaadin.flow.component.textfield.NumberField;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouterLayout;
-
-import org.vaadin.bpexampleapp.MainLayout;
-import org.vaadin.bpexampleapp.lists.*;
 import org.vaadin.bpexampleapp.model.ProductRepository;
 
-@Route(value="fh", layout = MainLayout.class)
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 
-public class FormContentHome  extends FormMainLayout {
+public class FormContentHome  extends VerticalLayout {
 	
-	private final ProductRepository repository;
 	final DatePicker dateOfAction = new DatePicker("Date");
 	final TextField nameOfAction = new TextField("Name");
 	
-	//public class FormContent  {
-		//VerticalLayout FormContent = new VerticalLayout();
-		//Select<String> actionType = new Select<String>("Action");
-		//actionType.setItems("Control", "Visit");
-		//FormContent.add(new Text("Home"));
-		//return FormContent;
-		
-	
 	
 	public FormContentHome(ProductRepository repository) {
-		this.repository = repository;
-		content.add(dateOfAction,nameOfAction);
+		add(dateOfAction,nameOfAction);
 		}
-	//}
 }
